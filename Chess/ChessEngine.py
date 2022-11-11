ab1f75b20bd62ca819e51bdae8151f19eda01c35
Pythadela <s.radomski19@gmail.com>
2022-11-11 19:05:13 +0100

    Added knight moves

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -85,7 +85,16 @@ class GameState:
                     break
 
     def getKnightMoves(self, row, col, moves):
-        pass
+        knight_moves = ((-2, -1), (-2, 1), (-1, 2), (1, 2), (2, -1), (2, 1), (-1, -2),
+                        (1, -2))
+        ally_color = "w" if self.white_to_move else "b"
+        for move in knight_moves:
+            end_row = row + move[0]
+            end_col = col + move[1]
+            if 0 <= end_row <= 7 and 0 <= end_col <= 7:
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != ally_color:
+                    moves.append(Move((row, col), (end_row, end_col), self.board))
 
     def getBishopMoves(self, row, col, moves):
         pass
