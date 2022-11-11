66b38bcbd9e37870ae37d84e3498e3c89ad00843
Pythadela <s.radomski19@gmail.com>
2022-11-11 18:57:20 +0100

    Draw chessboard and pieces

---
+++ Chess/ChessMain.py
@@ -0,0 +1,62 @@
+import pygame as p
+from Chess import ChessEngine
+import sys
+
+WIDTH = HEIGHT = 480
+DIMENSION = 8
+SQUARE_SIZE = HEIGHT // DIMENSION
+
+MAX_FPS = 15
+
+IMAGES = {}
+
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    game_state = ChessEngine.GameState()
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+                p.quit()
+                sys.exit()
+
+        clock.tick(MAX_FPS)
+        p.display.flip()
+        drawGameState(screen, game_state)
+
+
+def drawGameState(screen, game_state):
+    drawBoard(screen)
+    drawPieces(screen, game_state.board)
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            color = colors[((row + column) % 2)]
+            p.draw.rect(screen, color, p.Rect(column * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+
+def drawPieces(screen, board):
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            piece = board[row][column]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(column * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+
+if __name__ == "__main__":
+    main()
